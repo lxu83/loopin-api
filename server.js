ecdf7efdf0d63c5c1fc91dd0e6d8d2a1c054f2e2
zhisheng zhou <zz676@nyu.edu>
2016-07-07 18:23:25 -0700

    changes

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.all('/*', function(req, res, next) {
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: 'LoopInAuthentication' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
